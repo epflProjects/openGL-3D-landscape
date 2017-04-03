172dec08e8fed7870941ba71022f8921c5307f1a
bnanchen <nanchenbastian@gmail.com>
2017-04-03 18:55:25 +0200

    fshader in shape

--- landscape/screenquad/screenquad_fshader.glsl
+++ landscape/screenquad/screenquad_fshader.glsl
@@ -4,11 +4,14 @@ in vec2 uv;
 
 out vec3 color;
 
-uniform int pass;
-
 uniform sampler2D heightmap_tex;
 uniform float tex_width;
 uniform float tex_height;
+uniform int[] permutation;
+
+float GenerateFrameTexture(float position) {
+    return (float)permutation[p * 256] / 255.0f;
+}
 
 void main() {
     color = texture(heightmap_tex,uv).rgb;
