11fb9b97e3300ae81b6404d6e2da5f6fe73fb887
bnanchen <nanchenbastian@gmail.com>
2017-04-06 17:56:25 +0200

    Perlin Noise plutôt stylé

--- landscape/screenquad/screenquad.h
+++ landscape/screenquad/screenquad.h
@@ -7,12 +7,14 @@ class ScreenQuad {
         GLuint vertex_array_id_;        // vertex array object
         GLuint program_id_;             // GLSL shader program ID
         GLuint vertex_buffer_object_;   // memory buffer
-        GLuint texture_id_;             // texture ID
+        //GLuint texture_id_;             // texture ID
+        GLuint permutation_texture_id_;
 
         float screenquad_width_;
         float screenquad_height_;
 
-        int permutation_[256];
+        //int permutation_[256];
+        int p_[512];
 
         // source: http://stackoverflow.com/questions/20734774/random-array-generation-with-no-duplicates
         void shuffle(int *arr, size_t n) {
@@ -36,12 +38,56 @@ class ScreenQuad {
             this->screenquad_width_ = screenquad_width;
             this->screenquad_height_ = screenquad_height;
 
-            // set perumutation table
+            //set perumutation table
+            int permutation_ [256];
             for (int i = 0; i < 256; i++) {
-              this->permutation_[i] = (float)i;
+              permutation_[i] = (float)i;
             }
 
-            shuffle(this->permutation_, sizeof(permutation_)/sizeof(int));
+            shuffle(permutation_, sizeof(permutation_)/sizeof(int));
+
+  //           int permutation_ [] = { 151,160,137,91,90,15,
+  //  131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+  //  190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+  //  88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+  //  77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+  //  102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+  //  135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+  //  5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+  //  223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+  //  129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+  //  251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+  //  49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+  //  138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
+  //  };
+
+            for (int i=0; i < 256 ; i++) {
+                this->p_[i] = permutation_[i];
+                this->p_[256+i] = this->p_[i];
+              }
+
+            for (int i = 0; i < 256; i++) {
+              cout << permutation_[i] << endl;
+            }
+
+            // put the p array in texture
+            {
+                glGenTextures(1, &permutation_texture_id_);
+                glBindTexture(GL_TEXTURE_1D, permutation_texture_id_);
+                glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+                glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+                // create texture for the color attachment
+                // see Table.2 on
+                // khronos.org/opengles/sdk/docs/man3/docbook4/xhtml/glTexImage2D.xml
+                glTexImage1D(GL_TEXTURE_1D, 0, GL_R32F, screenquad_width_, screenquad_height_,
+                             GL_RED, GL_UNSIGNED_INT, this->p_);
+                // how to load from buffer
+
+            }
 
             // compile the shaders
             program_id_ = icg_helper::LoadShaders("screenquad_vshader.glsl",
@@ -97,12 +143,16 @@ class ScreenQuad {
                                       ZERO_BUFFER_OFFSET);
             }
 
-            // load/Assign texture
-            this->texture_id_ = texture;
-            glBindTexture(GL_TEXTURE_2D, texture_id_);
-            GLuint tex_id = glGetUniformLocation(program_id_, "heightmap_tex");
+            // // load/Assign texture
+            // this->texture_id_ = texture;
+            // glBindTexture(GL_TEXTURE_2D, texture_id_);
+            // GLuint tex_id = glGetUniformLocation(program_id_, "heightmap_tex");
+            // glUniform1i(tex_id, 0 /*GL_TEXTURE0*/);
+            // glBindTexture(GL_TEXTURE_2D, 0);
+            glBindTexture(GL_TEXTURE_1D, permutation_texture_id_);
+            GLuint tex_id = glGetUniformLocation(program_id_, "permutation_tex");
             glUniform1i(tex_id, 0 /*GL_TEXTURE0*/);
-            glBindTexture(GL_TEXTURE_2D, 0);
+            glBindTexture(GL_TEXTURE_1D, 0);
 
             // to avoid the current object being polluted
             glBindVertexArray(0);
@@ -115,7 +165,8 @@ class ScreenQuad {
             glDeleteBuffers(1, &vertex_buffer_object_);
             glDeleteProgram(program_id_);
             glDeleteVertexArrays(1, &vertex_array_id_);
-            glDeleteTextures(1, &texture_id_);
+            //glDeleteTextures(1, &texture_id_);
+            glDeleteTextures(1, &permutation_texture_id_);
         }
 
         void UpdateSize(int screenquad_width, int screenquad_height) {
@@ -135,7 +186,8 @@ class ScreenQuad {
 
             // bind texture
             glActiveTexture(GL_TEXTURE0);
-            glBindTexture(GL_TEXTURE_2D, texture_id_);
+            //glBindTexture(GL_TEXTURE_2D, texture_id_);
+            glBindTexture(GL_TEXTURE_1D, permutation_texture_id_);
 
             // draw
             glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
--- landscape/screenquad/screenquad_fshader.glsl
+++ landscape/screenquad/screenquad_fshader.glsl
@@ -1,32 +1,32 @@
 #version 330
 
 in vec2 uv;
+in vec3 vp;
 
 out vec3 color;
 
-uniform sampler2D heightmap_tex;
+uniform sampler1D permutation_tex;
 uniform float tex_width;
 uniform float tex_height;
-uniform int permutation[256];
 
 // Version from: http://http.developer.nvidia.com/GPUGems2/gpugems2_chapter26.html
-/*vec3 g[16] = vec3[](vec3(1,1,0), vec3(-1,1,0), vec3(1,-1,0), vec3(-1,-1,0),
-            vec3(1,0,1), vec3(-1,0,1), vec3(1,0,-1), vec3(-1,0,-1),
-            vec3(0,1,1), vec3(0,-1,1), vec3(0,1,-1), vec3(0,-1,-1),
-            vec3(1,1,0), vec3(0,-1,1), vec3(-1,1,0), vec3(0,-1,-1));
+// vec3 g[16] = vec3[](vec3(1,1,0), vec3(-1,1,0), vec3(1,-1,0), vec3(-1,-1,0),
+//             vec3(1,0,1), vec3(-1,0,1), vec3(1,0,-1), vec3(-1,0,-1),
+//             vec3(0,1,1), vec3(0,-1,1), vec3(0,1,-1), vec3(0,-1,-1),
+//             vec3(1,1,0), vec3(0,-1,1), vec3(-1,1,0), vec3(0,-1,-1));
+//
+// vec3 generateGradTexture(int position) {
+//     return g[position * 16];
+// }
+//
+// vec4 generatePermTexture(int position) {
+//     return vec4(permutation[position * 256] / 255.0f, // TODO not sure
+//                 permutation[position * 256] / 255.0f,
+//                 permutation[position * 256] / 255.0f,
+//                 permutation[position * 256] / 255.0f);
+// }
 
-vec3 generateGradTexture(int position) {
-    return g[position * 16];
-}
-
-vec4 generatePermTexture(int position) {
-    return vec4(permutation[position * 256] / 255.0f, // TODO not sure
-                permutation[position * 256] / 255.0f,
-                permutation[position * 256] / 255.0f,
-                permutation[position * 256] / 255.0f);
-}
-
-vec3 fade(vec3 t) {
+/*vec3 fade(vec3 t) {
     return t * t * t * (t * (t * 6 - 15) + 10);
 }
 
@@ -40,6 +40,33 @@ float grad(float x, vec3 p) {
 
 */
 // Version from: http://mrl.nyu.edu/~perlin/noise/
+int p[] = int[]( 151,160,137,91,90,15,
+ 131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+ 190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+ 88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+ 77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+ 102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+ 135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+ 5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+ 223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+ 129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+ 251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+ 49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+ 138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
+ );
+
+// int p[256];
+// for (int i = 0; i < 256; i++) {
+//   p[i] = float(i);
+// }
+//
+// for (i = 0; i < 256 - 1; i++) {
+//   int j = i + rand() / (RAND_MAX / (256 - i) + 1);
+//   int t = arr[j];
+//   arr[j] = arr[i];
+//   arr[i] = t;
+// }
+
 float fade(float t) {
     return t * t * t * (t * (t * 6 - 15) + 10);
 }
@@ -53,37 +80,67 @@ float grad(int hash, float x, float y, float z) {
     float u = h<8 ? x : y;
     float v = h<4 ? y : h==12 || h==14 ? x : z;
     return ((h&1) == 0 ? u : -u) + ((h&2) == 0 ? v : -v);
+
+    // int h = hash & 15;
+    // float u = 0.0f;
+    // if (h < 8) {
+    //   u = x;
+    // } else {
+    //   u = y;
+    // }
+    //
+    // float v = 0.0f;
+    // if (h < 4) {
+    //   v = y;
+    // } else if (h==12 || h==14) {
+    //   v = x;
+    // } else {
+    //   v = z;
+    // }
+    //
+    // if ((h & 1) != 0) {
+    //   u = -u;
+    // }
+    //
+    // if ((h & 2) != 0) {
+    //   v = -v;
+    // }
+    //
+    // return u + v;
 }
 
 float noise(float x, float y, float z) {
-    int X = floatBitsToInt(floor(x)) & 255;
-    int Y = floatBitsToInt(floor(y)) & 255;
-    int Z = floatBitsToInt(floor(z)) & 255;
-    x -= floor(x);
-    y -= floor(y);
-    z -= floor(z);
+    int X = int(floor(x)) & 255;
+    int Y = int(floor(y)) & 255;
+    int Z = int(floor(z)) & 255;
+
+    x = x - floor(x);
+    y = y - floor(y);
+    z = z - floor(z);
 
     float u = fade(x);
     float v = fade(y);
     float w = fade(z);
 
-    int A = permutation[X]+Y;
-    int AA = permutation[A]+Z;
-    int AB = permutation[A+1]+Z;
-    int B = permutation[X+1]+Y;
-    int BA = permutation[B]+Z;
-    int BB = permutation[B+1]+Z;
+    int A = p[X]+Y;//int(texture(permutation_tex, X))+Y;//permutation_tex[X]+Y;
+    int AA = p[A]+Z;//int(texture(permutation_tex, A))+Z;//permutation_tex[A]+Z;
+    int AB = p[A+1]+Z;//int(texture(permutation_tex, A+1))+Z;//permutation_tex[A+1]+Z;
+    int B = p[X+1]+Y;//int(texture(permutation_tex, X+1))+Y;//permutation_tex[X+1]+Y;
+    int BA = p[B]+Z;//int(texture(permutation_tex, B))+Z;//permutation_tex[B]+Z;
+    int BB = p[B+1]+Z;//int(texture(permutation_tex, B+1))+Z;//permutation_tex[B+1]+Z;
 
-    return lerp(w, lerp(v, lerp(u, grad(permutation[AA  ], x  , y  , z   ),     // AND ADD
-                                     grad(permutation[BA  ], x-1, y  , z   )),  // BLENDED
-                             lerp(u, grad(permutation[AB  ], x  , y-1, z   ),   // RESULTS
-                                     grad(permutation[BB  ], x-1, y-1, z   ))), // FROM  8
-                     lerp(v, lerp(u, grad(permutation[AA+1], x  , y  , z-1 ),   // CORNERS
-                                     grad(permutation[BA+1], x-1, y  , z-1 )),  // OF CUBE
-                             lerp(u, grad(permutation[AB+1], x  , y-1, z-1 ),
-                                     grad(permutation[BB+1], x-1, y-1, z-1 ))));
+    return lerp(w, lerp(v, lerp(u, grad(int(p[AA]), x  , y  , z   ),     // AND ADD
+                                     grad(int(p[BA]), x-1, y  , z   )),  // BLENDED
+                             lerp(u, grad(int(p[AB]), x  , y-1, z   ),   // RESULTS
+                                     grad(int(p[BB]), x-1, y-1, z   ))), // FROM  8
+                     lerp(v, lerp(u, grad(int(p[AA+1]), x  , y  , z-1 ),   // CORNERS
+                                     grad(int(p[BA+1]), x-1, y  , z-1 )),  // OF CUBE
+                             lerp(u, grad(int(p[AB+1]), x  , y-1, z-1 ),
+                                     grad(int(p[BB+1]), x-1, y-1, z-1 ))));
 }
 
 void main() {
-    color = vec3(noise(uv.x, uv.y, 0)); // TODO not sure of the zero for z
+    //color = texture(heightmap_tex,uv).rgb;
+     color = vec3(noise(uv.x*10, uv.y*10, 0)); // TODO not sure of the zero for z
+    //color = vec3(grad(3,uv.x, uv.y, 0));
 }
