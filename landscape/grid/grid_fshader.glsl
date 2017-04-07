d8e94fc7fc4354a9d36e455f775c1b5df54d937a
Cédric Viaccoz <cedric.viaccoz@gmail.com>
2017-04-07 14:39:18 +0200

    WIP of diffuse shading of the grid

--- landscape/grid/grid_fshader.glsl
+++ landscape/grid/grid_fshader.glsl
@@ -7,14 +7,30 @@ out vec3 color;
 
 uniform sampler2D tex;
 
-void main() {
-    vec3 vcolor = vec3(0.0f, 0.0f, 0.0f);
+float height(vec2 xy){
+	return texture(tex, xy).z;
+}
 
-    // compute triangle normal using dFdx and dFdy
+void main() {
+    
+    //still need some adaptation and engineering, the result is fare from beign perfect.
+    vec3 vcolor = vec3(0.0f, 0.0f, 0.0f);
+    vec3 off = vec3(1.0f, 1.0f, 0.0f);
+    vec3 P = vpoint_mv.xyz;
+
+	float hL = height(P.xy - off.xz);
+	float hR = height(P.xy + off.xz);
+	float hD = height(P.xy - off.zy);
+	float hU = height(P.xy + off.zy);
+
+	vec3 N = vec3(hL-hR, hD - hU, 2.0f);
+	color = normalize(N);
+
+    /*// compute triangle normal using dFdx and dFdy
     vec3 fragVertexEc = vpoint_mv.xyz;
     vec3 x = dFdx(fragVertexEc);
     vec3 y = dFdy(fragVertexEc);
     vec3 triangle_normal = normalize(cross(x, y));
 
-    color = triangle_normal;
+    color = triangle_normal;*/
 }
