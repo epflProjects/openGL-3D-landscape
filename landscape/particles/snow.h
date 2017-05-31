6a970a27fc8e4c58fbe8ca55272525ce1cdedb8e
bnanchen <nanchenbastian@gmail.com>
2017-05-31 17:20:38 +0200

    Deletion of CameraRight_wordlspace_ID, CameraUp_worldspace_ID and ViewProjMatrixID

--- landscape/particles/snow.h
+++ landscape/particles/snow.h
@@ -29,9 +29,6 @@ GLuint snowflakes_position_buffer;
 GLuint snowflakes_color_buffer;
 int SnowflakesCount;
 int LastUsedSnowFlake = 0;
-GLuint CameraRight_worldspace_ID;
-GLuint CameraUp_worldspace_ID;
-GLuint ViewProjMatrixID;
 
 class Snow {
 
@@ -68,16 +65,6 @@ public:
     GLuint program_id_ =
         icg_helper::LoadShaders("snow_vshader.glsl", "snow_fshader.glsl");
 
-    // Vertex shader
-    CameraRight_worldspace_ID =
-        glGetUniformLocation(program_id_, "CameraRight_worldspace");
-    CameraUp_worldspace_ID =
-        glGetUniformLocation(program_id_, "CameraUp_worldspace");
-    ViewProjMatrixID = glGetUniformLocation(program_id_, "VP");
-
-    // fragment shader
-    GLuint TextureID = glGetUniformLocation(program_id_, "myTextureSampler");
-
     for (int i = 0; i < MaxSnowFlakes; i++) {
       SnowFlakesContainer[i].life = -1.0f;
       SnowFlakesContainer[i].cameradistance = -1.0f;
@@ -85,8 +72,8 @@ public:
 
     // VBO containing the 4 vertices of the snowflakes
     static const GLfloat g_vertex_buffer_data[] = {
-        -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f,
-        -0.5f, 0.5f,  0.0f, 0.5f, 0.5f,  0.0f,
+        -0.5f, -0.5f, 0.0f, 0.05f, -0.5f, 0.0f,
+        -0.5f, 0.5f,  0.0f, 0.5f,  0.5f,  0.0f,
     };
     GLuint billboard_vertex_buffer;
     glGenBuffers(1, &billboard_vertex_buffer);
@@ -115,102 +102,90 @@ public:
     glm::vec3 CameraPosition = cameraPos;
     glm::mat4 ViewProjectionMatrix = viewProjectionMatrix;
 
-    int newsnowflakes = 1; //(int)(deltaTime * 10000.0);
+    int newsnowflakes = (int)(deltaTime * 10000.0);
     if (newsnowflakes > (int)(0.016f * 10000.0)) {
       newsnowflakes = (int)(0.016f * 10000.0);
     }
 
-    // for (int i = 0; i < newsnowflakes; i++) {
-    //   int snowflakeIndex = FindUnusedSnowflake();
-    //   SnowFlakesContainer[snowflakeIndex].life =
-    //       5.0f; // this snowflake will live 5 seconds
-    //   SnowFlakesContainer[snowflakeIndex].pos = glm::vec3(0, 0, 0.0f);
-    //
-    //   float spread = 0.2f;
-    //   glm::vec3 maindir = glm::vec3(0.0f, 0.0f, -10.0f);
-    //
-    //   // bad way to create random direction. See:
-    //   //
-    //   http://stackoverflow.com/questions/5408276/python-uniform-spherical-distribution
-    //   // glm::vec3 randomdir = glm::vec3((rand() % 2000 - 1000.0f) / 1000.0f,
-    //   //                                 (rand() % 2000 - 1000.0f) / 1000.0f,
-    //   //                                 (rand() % 2000 - 1000.0f) /
-    //   1000.0f);
-    //
-    //   SnowFlakesContainer[snowflakeIndex].speed =
-    //       glm::vec3(0.0f, 0.0f, 0.0f); // maindir /*+ randomdir*/ * spread;
-    //
-    //   // // Uniform Spherical Distribution
-    //   // unsigned seed =
-    //   //     std::chrono::system_clock::now().time_since_epoch().count();
-    //   // std::mt19937 generator(seed);
-    //   // std::uniform_real_distribution<double> uniform01(0.0, 1.0);
-    //   // double theta = 2 * M_PI * uniform01(generator);
-    //   // double phi = acos(1 - 2 * uniform01(generator));
-    //   // double x = sin(phi) * cos(theta);
-    //   // double y = sin(phi) * sin(theta);
-    //   // double z = cos(phi);
-    //   // glm::vec3 USD(x, y, z);
-    //   // ParticlesContainer[particleIndex].speed = maindir + USD * spread;
-    //
-    //   // random color
-    //   SnowFlakesContainer[snowflakeIndex].r = 200;
-    //   SnowFlakesContainer[snowflakeIndex].g = 200;
-    //   SnowFlakesContainer[snowflakeIndex].b = 200;
-    //   SnowFlakesContainer[snowflakeIndex].a = 1.0;
-    //
-    //   SnowFlakesContainer[snowflakeIndex].size =
-    //       1.0f /*(rand() % 1000) / 2000.0f + 0.1f*/;
-    // }
+    for (int i = 0; i < newsnowflakes; i++) {
+      int snowflakeIndex = FindUnusedSnowflake();
+      SnowFlakesContainer[snowflakeIndex].life =
+          5.0f; // this snowflake will live 5 seconds
+      SnowFlakesContainer[snowflakeIndex].pos = glm::vec3(0, 0, -20.0f);
+
+      float spread = 1.5f;
+      glm::vec3 maindir = glm::vec3(0.0f, 10.0f, 0.0f);
+
+      // bad way to create random direction. See:
+      // http://stackoverflow.com/questions/5408276/python-uniform-spherical-distribution
+      glm::vec3 randomdir = glm::vec3((rand() % 2000 - 1000.0f) / 1000.0f,
+                                      (rand() % 2000 - 1000.0f) / 1000.0f,
+                                      (rand() % 2000 - 1000.0f) / 1000.0f);
+
+      SnowFlakesContainer[snowflakeIndex].speed = maindir + randomdir * spread;
+
+      // // Uniform Spherical Distribution
+      // unsigned seed =
+      //     std::chrono::system_clock::now().time_since_epoch().count();
+      // std::mt19937 generator(seed);
+      // std::uniform_real_distribution<double> uniform01(0.0, 1.0);
+      // double theta = 2 * M_PI * uniform01(generator);
+      // double phi = acos(1 - 2 * uniform01(generator));
+      // double x = sin(phi) * cos(theta);
+      // double y = sin(phi) * sin(theta);
+      // double z = cos(phi);
+      // glm::vec3 USD(x, y, z);
+      // ParticlesContainer[particleIndex].speed = maindir + USD * spread;
+
+      // random color
+      SnowFlakesContainer[snowflakeIndex].r = 200;
+      SnowFlakesContainer[snowflakeIndex].g = 200;
+      SnowFlakesContainer[snowflakeIndex].b = 200;
+      SnowFlakesContainer[snowflakeIndex].a = 1.0;
+
+      SnowFlakesContainer[snowflakeIndex].size =
+          (rand() % 1000) / 2000.0f + 0.1f;
+    }
 
     // Simulate all particles
-    // int SnowflakesCount = 0;
-    // for (int i = 0; i < MaxSnowFlakes; i++) {
-    //   Snowflake &s = SnowFlakesContainer[i];
-    //
-    //   if (s.life > 0.0f) {
-    //     s.life -= deltaTime;
-    //     if (s.life > 0.0f) {
-    //       // s.speed = glm::vec3(0.0f, -9.81f, 0.0f) * (float)deltaTime *
-    //       0.5f; s.pos += s.speed * (float)deltaTime; s.cameradistance =
-    //       glm::length(s.pos - CameraPosition);
-    //
-    //       g_snowflake_position_size_data[4 * SnowflakesCount + 0] = s.pos.x;
-    //       g_snowflake_position_size_data[4 * SnowflakesCount + 1] = s.pos.y;
-    //       g_snowflake_position_size_data[4 * SnowflakesCount + 2] = s.pos.z;
-    //       g_snowflake_position_size_data[4 * SnowflakesCount + 3] = s.size;
-    //
-    //       g_snowflake_color_data[4 * SnowflakesCount + 0] = s.r;
-    //       g_snowflake_color_data[4 * SnowflakesCount + 1] = s.g;
-    //       g_snowflake_color_data[4 * SnowflakesCount + 2] = s.b;
-    //       g_snowflake_color_data[4 * SnowflakesCount + 3] = s.a;
-    //
-    //     } else {
-    //       // Dead particles to sort buffer
-    //       s.cameradistance = -1.0f;
-    //     }
-    //
-    //     SnowflakesCount++;
-    //   }
-    // }
+    int SnowflakesCount = 0;
+    for (int i = 0; i < MaxSnowFlakes; i++) {
+      Snowflake &s = SnowFlakesContainer[i];
 
-    // sortSnowflakes();
+      if (s.life > 0.0f) {
+        s.life -= deltaTime;
+        if (s.life > 0.0f) {
+          s.speed += glm::vec3(0.0f, -9.81f, 0.0f) * (float)deltaTime * 0.5f;
+          s.pos += s.speed * (float)deltaTime;
+          s.cameradistance = glm::length(s.pos - CameraPosition);
 
-    // snowCount = SnowflakesCount;
+          g_snowflake_position_size_data[4 * SnowflakesCount + 0] = s.pos.x;
+          g_snowflake_position_size_data[4 * SnowflakesCount + 1] = s.pos.y;
+          g_snowflake_position_size_data[4 * SnowflakesCount + 2] = s.pos.z;
+          g_snowflake_position_size_data[4 * SnowflakesCount + 3] = s.size;
+
+          g_snowflake_color_data[4 * SnowflakesCount + 0] = s.r;
+          g_snowflake_color_data[4 * SnowflakesCount + 1] = s.g;
+          g_snowflake_color_data[4 * SnowflakesCount + 2] = s.b;
+          g_snowflake_color_data[4 * SnowflakesCount + 3] = s.a;
+
+        } else {
+          // Dead particles to sort buffer
+          s.cameradistance = -1.0f;
+        }
+
+        SnowflakesCount++;
+      }
+    }
+
+    sortSnowflakes();
+
+    snowCount = SnowflakesCount;
 
     // Mise à jour des tampons qu'OpenGL utilise pour le rendu.
     // Il y a des façons bien plus sophistiquées pour envoyer des données du
     // CPU au GPU, mais c'est en dehors du champ de ce tutoriel.
     // http://www.opengl.org/wiki/Buffer_Object_Streaming
-
-    // Same as the billboards tutorial
-    glUniform3f(CameraRight_worldspace_ID, ViewProjectionMatrix[0][0],
-                ViewProjectionMatrix[1][0], ViewProjectionMatrix[2][0]);
-    glUniform3f(CameraUp_worldspace_ID, ViewProjectionMatrix[0][1],
-                ViewProjectionMatrix[1][1], ViewProjectionMatrix[2][1]);
-
-    glUniformMatrix4fv(ViewProjMatrixID, 1, GL_FALSE,
-                       &ViewProjectionMatrix[0][0]);
 
     glBindBuffer(GL_ARRAY_BUFFER, snowflakes_position_buffer);
     glBufferData(GL_ARRAY_BUFFER, MaxSnowFlakes * 4 * sizeof(GLfloat), NULL,
